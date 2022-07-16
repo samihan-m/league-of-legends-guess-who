1ae8cad82d19a78ea7537d6cf9cb6d3d02e8a147
PikaGoku <ssmuppirala@gmail.com>
2022-07-16 11:37:56 -0700

    Added credit link to me :)

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import Board from './Board.js';
 
 function App() {
   return (
-    <div className="App"> 
-      <h1 className={"centered"}>Guess Who? LoL Edition</h1>
+    <div className="centered App"> 
+      <h1>Guess Who? LoL Edition</h1>
       <br/>
       <Board />
+      <br/>
+      <div id="credits">
+        <a href="https://github.com/samihan-m">by samihan-m</a>
+      </div>
+      <br/>
     </div>
   );
 }
